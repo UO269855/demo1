a393a8c8500b0fa6f50cc2e3d27e947a615a7a92
Álex Caso <uo269855@uniovi.es>
2021-09-23 16:03:12 +0200

    cambio3 toca esta linea

--- src/Demo.java
+++ src/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	//arreglado el token de git
 	public static void main(String[] args) {
 		System.out.println("buenas tardes");
-		System.out.println("buenas tardes 28");
+		System.out.println("cambio 3 que mal eh");
 		System.out.println("buenas tardes 346");
 	}
 }
