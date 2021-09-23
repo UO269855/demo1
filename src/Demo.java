4a4f184e70e93928b520ab5089d496f930674b99
Álex Caso <uo269855@uniovi.es>
2021-09-23 16:04:35 +0200

    masster commit

--- src/Demo.java
+++ src/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
 	//arreglado el token de git
 	public static void main(String[] args) {
 		System.out.println("buenas tardes");
-		System.out.println("buenas tardes 28");
+		System.out.println("uf qué linea mas fea esta de master");
 		System.out.println("buenas tardes 346");
+		System.out.println("uf qué linea mas fea es esta tambien");
 	}
 }
